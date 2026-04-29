6daab9a89a64352f93f94a1e27a103149ec90c95
PhanManhTan <manhtan06120@gmail.com>
2026-04-30 05:36:35 +0700

    Finish back end login signin forgetpass setnewpass

--- app/src/main/java/com/example/myapplication/data/remote/RetrofitClient.java
+++ app/src/main/java/com/example/myapplication/data/remote/RetrofitClient.java
@@ -1,39 +1,93 @@
+//package com.example.myapplication.data.remote;
+//
+//import com.example.myapplication.utils.Constants;
+//import okhttp3.OkHttpClient;
+//import okhttp3.Request;
+//import okhttp3.logging.HttpLoggingInterceptor;
+//import retrofit2.Retrofit;
+//import retrofit2.converter.gson.GsonConverterFactory;
+//
+//public class RetrofitClient {
+//    private static Retrofit retrofit = null;
+//
+//    public static Retrofit getClient(this) {
+//        if (retrofit == null) {
+//            // Thêm Logging Interceptor để xem chi tiết Request/Response
+//            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+//            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+//
+//            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                    .addInterceptor(logging) // Add logging
+//                    .addInterceptor(chain -> {
+//                        Request original = chain.request();
+//                        Request request = original.newBuilder()
+//                                .header("apikey", Constants.SUPABASE_API_KEY)
+//                                .header("Authorization", "Bearer " + Constants.SUPABASE_API_KEY)
+//                                .method(original.method(), original.body())
+//                                .build();
+//                        return chain.proceed(request);
+//                    })
+//                    .build();
+//
+//            retrofit = new Retrofit.Builder()
+//                    .baseUrl(Constants.SUPABASE_URL)
+//                    .client(okHttpClient)
+//                    .addConverterFactory(GsonConverterFactory.create())
+//                    .build();
+//        }
+//        return retrofit;
+//    }
+//}
 package com.example.myapplication.data.remote;
 
+import android.content.Context;
+
 import com.example.myapplication.utils.Constants;
+import com.example.myapplication.utils.SessionManager;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit = null;
 
-    public static Retrofit getClient() {
-        if (retrofit == null) {
+    public static Retrofit getClient(Context context) {
 
-            // Create HttpClient
-            OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                    .addInterceptor(chain -> {
-                        // Add Auth Header for every request
-                        Request original = chain.request();
-                        Request request = original.newBuilder()
-                                .header("apikey", Constants.SUPABASE_API_KEY)
-                                .header("Authorization", "Bearer " + Constants.SUPABASE_API_KEY)
-                                .method(original.method(), original.body())
-                                .build();
+        // Logging để debug
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-                        return chain.proceed(request);
-                    })
-                    .build();
+        SessionManager sessionManager = new SessionManager(context);
 
-            // Connect with the header created
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.SUPABASE_URL)
-                    .client(okHttpClient)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        }
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .addInterceptor(chain -> {
+                    Request original = chain.request();
+
+                    String token = sessionManager.getToken(); // 🔥 lấy access_token
+
+                    Request.Builder builder = original.newBuilder()
+                            .header("apikey", Constants.SUPABASE_API_KEY)
+                            .header("Content-Type", "application/json");
+
+                    // 🔥 CHỈ thêm Authorization nếu có token
+                    if (token != null) {
+                        builder.header("Authorization", "Bearer " + token);
+                    }
+
+                    return chain.proceed(builder.build());
+                })
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.SUPABASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
         return retrofit;
     }
 }
